c43aa7ac9084d9c58187cdf6def0a2a608909cf2
408737046 <134588819+408737046@users.noreply.github.com>
2024-04-29 11:50:09 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var captureGraphics
 var capture_width = 640
 var capture_height = 480
 var span = 5
+var radioElement
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,8 +11,17 @@ function setup() {
   captureGraphics = createGraphics(capture_width,capture_height)
   captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
-  capture.hide
+  capture.hide()
   //---旋鈕介面---//
+  var radioElement = createRadio();
+  radioElement.position(width/2-300,20)
+  radioElement.option("方塊")
+  radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
+  radioElement.style("color","#fff")
+  radioElement.style("font-size","40px")
 }
 
 function draw() {
@@ -25,7 +35,37 @@ function draw() {
       for(var y= 0;y<captureGraphics.height;y=y+span){
         var pixel = captureGraphics.get(x,y)
         fill(pixel)
-        rect(x,y,span)
+        if(radioElement.value()=="方塊" || radioElement.value()==""){
+          rect(x,y,span)
+        }
+        if(radioElement.value()=="圓圈"){
+          ellipse(x,y,span)
+        }
+        if(radioElement.value()=="亮度"){
+          bk = (pixel[0]+pixel[1]+pixel[2])/3
+          fill(bk)
+          //ellipse(x,y,span)  //span代表圓圈的直徑
+          ellipse(x,y,span*map(bk,0,255,0,1))  //透過mao指令，bk在0~255間，依照比率設定0~1間的數字
+        }
+        if(radioElement.value()=="紅底"){
+          colorMode(HSB)
+          fill(pixel[0],80,80)
+          rectMode(CENTER)
+          rect(0,0,span*0.6)
+          fill(0)
+          ellipse(0,0,10)
+        pop()
+        }
+        if(radioElement.value()=="文字"){
+          const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+          let txt = "一二三四五田雷電龕龘"
+          bk = (pixel[0]+pixel[1]+pixel[2])/3
+          let bkId = int(map(bk, 0, 255, 9, 0))
+          text(txt[bkId])
+          textSize(span)
+          textStyle(BOLD)
+          text(txt[bkId],x,y)
+        }
       }
     }
     pop()
